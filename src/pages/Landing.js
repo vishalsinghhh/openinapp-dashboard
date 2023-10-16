3f09ecb6943d6127e811295be7618bbd8ecfbf14
vishalsinghhh <vishalsingh0710vs@gmail.com>
2023-10-16 21:40:21 +0530

    sign in

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
+import { AiFillApple } from "react-icons/ai";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
@@ -11,6 +12,8 @@ import discord from "../images/discord.svg";
 
 const Landing = () => {
   const navigate = useNavigate();
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   const googleProvider = new GoogleAuthProvider();
   const GoogleLogin = async () => {
     try {
@@ -39,14 +42,16 @@ const Landing = () => {
               <img src={twitter} alt="" className="imgMar img" />
             </div>
             <div>
-              <img src={linkedin} alt="" className="img"/>
+              <img src={linkedin} alt="" className="img" />
             </div>
             <div>
-              <img src={discord} alt="" className="img"/>
+              <img src={discord} alt="" className="img" />
             </div>
           </div>
         </div>
-        <div>
+        <div className="singIn">
+          <div>Sign In</div>
+          <div>Sign in to your account</div>
           <button
             onClick={() => {
               GoogleLogin();
@@ -54,6 +59,26 @@ const Landing = () => {
           >
             <FcGoogle /> Sign in with Google
           </button>
+          <button
+            onClick={() => {
+              GoogleLogin();
+            }}
+          >
+            <AiFillApple /> Sign in with Apple
+          </button>
+          <div>
+            <div>
+              Email address
+              <input type="text" onChange={(e)=>{setEmail(e.target.value)}}/>
+            </div>
+            <div>
+              Password
+              <input type="password" onChange={(e)=>{setPassword(e.target.value)}}/>
+            </div>
+            Forgot password?
+            <button>Sign In</button>
+          </div>
+          Don't have an account? Register here
         </div>
       </div>
     </>
