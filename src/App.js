20af8834a1e5508f2ff9546fe166f00abce48b11
vishalsinghhh <vishalsingh0710vs@gmail.com>
2023-10-16 19:12:24 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Landing from './pages/Landing';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-    Hello
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path={'/'} element={<Landing/>}/>
+      </Routes>
+      <Routes>
+        <Route exact path={'/dashboard'} element={<Dashboard/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
