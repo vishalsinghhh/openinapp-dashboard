d15120a535df1980079f2047a06eb9c1a76e4ed3
vishalsinghhh <vishalsingh0710vs@gmail.com>
2023-10-16 19:37:07 +0530

    google login

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { FcGoogle } from 'react-icons/fc';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import {auth} from '../utils/firebase'
 
 const Landing = () => {
+    const googleProvider = new GoogleAuthProvider()
+    const GoogleLogin = async()=>{
+        try {
+            const result = await signInWithPopup(auth, googleProvider)
+            console.log(result.user);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
   return (
-    <div>Landing</div>
+    <div>
+    <div><button onClick={()=>{GoogleLogin()}}><FcGoogle/> Sign in with Google</button></div>
+    </div>
   )
 }
 
